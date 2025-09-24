cd248abd8170b302830f4ee8d941c571cb1008d4
Nitin Kumar Dwivedi <nitindwivedi.dev@gmail.com>
2025-09-24 12:06:13 +0530

    issue is to be fixed

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import {Route, Routes,useLocation} from 'react-router-dom'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetails from './Pages/MovieDetails'
+import SeatLayout from './Pages/SeatLayout'
+import MyBookings  from './Pages/MyBookings'
+import Favorite from './Pages/Favorite'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
+
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+   <>
+   <Toaster/>
+      {!isAdminRoute && <Navbar/> } 
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/favorite' element={<Favorite/>}/>
+      </Routes>
+      {!isAdminRoute && <Footer/> }
+
+   </>
+  )
+}
+
+export default App
