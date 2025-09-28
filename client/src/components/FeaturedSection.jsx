ccd19fea4f6cf40ef65e3e2d956b69c6860cd495
Nitin Kumar Dwivedi <nitindwivedi.dev@gmail.com>
2025-09-28 22:44:34 +0530

    Fixed the issue with featuredsection.

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -2,13 +2,16 @@ import { ArrowRight } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import BlurCircle from "./BlurCircle";
+import MovieCard from "./MovieCard";
+
 import { dummyShowsData } from "../assets/assets";
+
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow:hidden">
+    <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
       <div className="relative flex items-center justify-between pt-20 pb-10">
         <BlurCircle top="0" right="-80px" />
 
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -2,17 +2,17 @@ import { StarIcon } from "lucide-react";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const MovieCard = () => {
+const MovieCard = ({movie}) => {
   const navigate = useNavigate();
 
   return (
     <div className="flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-66">
       <img
         onClick={() => {
-          navigate("/movies/${movie._id}");
+          navigate(`/movies/${movie._id}`);
           scrollTo(0, 0);
         }}
-        src={movies.backdrop_path}
+        src={movie.backdrop_path}
         alt=""
         className="rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer"
       />
