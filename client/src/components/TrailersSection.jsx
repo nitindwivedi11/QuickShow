a972b7d7bfc469a73b4ef70b6fcb3a4ba0528b39
Nitin Kumar Dwivedi <nitindwivedi.dev@gmail.com>
2025-09-30 23:14:57 +0530

    Started working on TrailerSection

---
+++ client/src/components/TrailersSection.jsx
@@ -0,0 +1,23 @@
+import React ,{useState} from 'react'
+import ReactPlayer from 'react-player'
+import {dummyTrailers} from '../assets/assets'
+import BlurCircle from './BlurCircle'
+
+const TrailersSection = () => {
+
+  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44 py-20 overflow-hidden'>
+      <p className='text-gray-300 font-medium text-lg max-w-[960px] mx-auto'>Trailers</p>
+
+      <div className='relative mt-6'>
+        <ReactPlayer url={currentTrailer.videoUrl} controls={false}  
+        className='mx-auto max-w-full' width="960px" height="540px"/>
+      </div>
+      
+    </div>
+  )
+}
+
+export default TrailersSection
