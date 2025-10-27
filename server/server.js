c2995dfac98215d4d88f37aabcd2335c38ff893b
Nitin Kumar Dwivedi <nitindwivedi.dev@gmail.com>
2025-10-27 23:57:00 +0530

    Backend Started

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = 3000;
+
+// middleware 
+app.use(express.json())
+app.use(cors())
+
+// api route endpoint
+app.get('/',(req,res)=>res.send('server is live '))
+
+// start backend server (app)
+app.listen(port,()=> console.log(`server listening at http://localhost:${port}`));
