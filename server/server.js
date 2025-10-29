b06a89241de8c834d0ad7c699515e62823ea0622
Nitin Kumar Dwivedi <nitindwivedi.dev@gmail.com>
2025-10-29 11:40:41 +0530

    Deploying on vercel

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './configs/db.js'; // importing connectDB 
+import { clerkMiddleware } from '@clerk/express' // this is clerk middleware 
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = 3000;
 
-await c
+await connectDB()
 
 // middleware 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 // api route endpoint
 app.get('/',(req,res)=>res.send('server is live '))
+app.use('/api/inngest',serve({client:inngest, functions})) // http endpoint for inngest
+
 
 // start backend server (app)
 app.listen(port,()=> console.log(`server listening at http://localhost:${port}`));
